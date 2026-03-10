acee2b6c30ce862c05145ed6a852e6dc2bdb6788
SAA-J <j.arayasancho28@ncd.edu>
2026-03-09 20:52:38 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#FFF8F0] flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-[#166534]">Happy Banyan</h1>
+    </div>
+  )
+}
+
+export default App
